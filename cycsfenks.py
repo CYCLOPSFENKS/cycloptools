0fee429d946ac74ce287cc525b97550b38098df4
cyclopsfenks123123 <67316427+cyclopsfenks123123@users.noreply.github.com>
2020-06-27 11:49:31 +0300

    Update cycsfenks.py

--- cycsfenks.py
+++ cycsfenks.py
@@ -3,3 +3,7 @@
 
 print("UÇBİRİM GÜNCELLEME[1]\n CYCLOP DDOS[2]\n GÖRÜNMEZ OLMAK[3]\n KONUM TESPİTİ[4]\n KAMERAYA SIZMA[5] ")
 seçim = int(input("İŞLEN NUMARANIZI GİRİN:"))
+
+if seçim == 1:
+  print("MASAÜTÜSÜNE GİDİOR AMQ")
+  cd /Masaüstü
