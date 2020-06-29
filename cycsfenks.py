c3b7190538459ba96ac65e2800eacb2ab996ff3c
cyclopsfenks123123 <67316427+cyclopsfenks123123@users.noreply.github.com>
2020-06-29 13:00:53 +0300

    Update cycsfenks.py

--- cycsfenks.py
+++ cycsfenks.py
@@ -1,1 +1,14 @@
+kullanıcı = "cyclop"
+şifre = "sfenks"
 
+def kullanıcı input("KULLANICI ADINIZ:")
+def şifre input("ŞİFRENİZ:")
+
+While True:
+
+if kullanıcı != defkullanıcı  or şifre != defşifre
+echo -e '\033[31;40;1m   KULLANICI ADINIZ VEYA ŞİFRENİZ YANLIŞ'
+
+else 
+echo -e 'HOŞ GELDİNİZ'
+break
